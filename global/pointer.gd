b4774111cefc8af1a225abc071632cd33583e9b2
lotseven <imlotseven@gmail.com>
2025-06-28 14:39:01 -0400

    restructured it a bit, added firing handling

--- global/pointer.gd
+++ global/pointer.gd
@@ -1,20 +1,49 @@
 extends Node2D
-signal clickCoords(Vector2)
+
+signal clickLeft(position: Vector2)
+signal clickRight(position: Vector2)
+
 @export var myScale = 1.2
+
+var isLeftHeld := false
+var isRightHeld := false
 
 func _ready():
 	scale.x = myScale
 	scale.y = myScale
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)  # Hide default cursor
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
-func _process(delta):
-	self.position = get_global_mouse_position()
+func _process(_delta):
+	position = get_global_mouse_position()
 
-func _unhandled_input(event):
-	if event is InputEventMouseButton and event.pressed:
-		#print("Mouse clicked at: ", event.position, "+ (printed from pointer.gd)")
-		doClickAnimation()
-		clickCoords.emit(event.position)
-		
-func doClickAnimation():
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			isLeftHeld = event.pressed
+			if event.pressed:
+				on_left_click(event.position)
+				
+		elif event.button_index == MOUSE_BUTTON_RIGHT:
+			isRightHeld = event.pressed
+			if event.pressed:
+				on_right_click(event.position)
+
+func _physics_process(_delta):
+	if isLeftHeld:
+		on_left_hold(position)
+	if isRightHeld:
+		on_right_hold(position)
+
+func on_left_click(position: Vector2):
+	clickLeft.emit(position)
+
+func on_right_click(position: Vector2):
+	clickRight.emit(position)
+
+func on_left_hold(position: Vector2):
+	#print("held L")
+	pass
+
+func on_right_hold(position: Vector2):
+	#print("held r")
 	pass
--- player/magGun.gd
+++ player/magGun.gd
@@ -1,8 +1,45 @@
 extends Node2D
 var bulletList
 @export var maxBullets = 3
+var aimMode = false
+var rightHoldTime := 0.0
+const AIM_HOLD_THRESHOLD := 0.5  # seconds
+
 func _ready() -> void:
-	Pointer.clickCoords.connect(clickRcv)
+	Pointer.clickLeft.connect(clickRcvL)
+	Pointer.clickRight.connect(clickRcvR)
+	# YOU CAN ALSO DO Pointer.isLeftHeld & Pointer.isRightHeld for mouse ins
 	
-func clickRcv(coords):
-	print("clicked @: ", coords, " (printed from magGun.gd)")
+func _process(delta: float) -> void:
+	manageAimingMode(delta)
+	
+func manageAimingMode(delta: float) -> void:
+	if Pointer.isRightHeld: # waits for half a second of holding before entering aim mode
+		rightHoldTime += delta
+		if rightHoldTime >= AIM_HOLD_THRESHOLD and !aimMode:
+			aimMode = true
+			print("Aim mode activated", " (printed from magGun.gd)")
+			
+	else: # if player releases aim button, magnet should fire off
+		rightHoldTime = 0.0
+		if aimMode:
+			handleAimRelease()
+		aimMode = false
+		
+func handleAimRelease():
+	# the vector x componant is expected behavior
+	# vectory y componant has positive y = down direction, since all graphics programs
+	# are kind of retarded in that way but thats ok i guess
+	var fireCoords = Pointer.position  # Cursor's position in world space
+	var fireVector = (fireCoords - global_position).normalized()
+	print("Aim mode deactivated: fire with vector: ", fireVector, " (printed from magGun.gd)")
+	
+	
+	
+func clickRcvL(coords): #does nothing rn
+	#print("left clicked @: ", coords, " (printed from magGun.gd)")
+	pass
+	
+func clickRcvR(coords): #does nothing rn
+	#print("right clicked @: ", coords, " (printed from magGun.gd)")
+	pass
