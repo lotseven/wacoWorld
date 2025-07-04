0b5ddef0b4f3c7feed169a159a2c1bda143f94fe
lotseven <imlotseven@gmail.com>
2025-07-04 19:27:35 -0400

    mechanic more fleshed out

--- player/player.gd
+++ player/player.gd
@@ -6,30 +6,31 @@ const SPEED := 12000.0
 const JUMP_FORCE := -300.0
 const GRAVITY := 980.0 #change to force
 const MASS = 80
-const SNAP_DISTANCE = 45.0
-const MAGNET_FORCE = 500000.0
+
 # cam stuff
 var camOffset = 600
 var camOffSaved = 600
 # references to children or other nodes
 @onready var sprite = $Sprite2D
 @onready var groundHitbox = $groundHitbox
-@onready var magHitbox = $magHitbox
 # magnet numbers
-#var activeMagnetList
-var magAcc = 3
 var maxMags = 3
+const MAGNET_FORCE = 280000.0
+const SNAP_DISTANCE = 35.0
 # sound
 var footstepSound = "res://sounds/playerSfx/steps.mp3"
-var flingMag
+# moving on magnets stuff
+var flingMag 
 var wantsToPull = false
-
+var wantsToPush = false
+var distanceScale = 1
+var distScaleConst = 300 # since distance scale must inversely relate to speed, i use 1/dist, which might be really small. this scales that back up
+var slopeScale = 3 # the base of the log that determines the scaling of speed/distance
 func _ready() -> void:
 	$magManager.connect("magChange", Callable(self, "updateMagMovement"))
 	camOffset = camOffSaved
 	$camera.offset = Vector2(0,-camOffset)
 	$camera.position = position
-	#activeMagnetList = $magManager.movementMags
 
 func _physics_process(delta):
 		updateMovementIntent()
@@ -46,20 +47,23 @@ func groundedMovement(delta):
 	var inputDir = 0.0
 	var airFactor = 1.0
 	if !is_on_floor(): airFactor = .5
+	
 	if Input.is_action_pressed("left"):
 		velocity.x = -SPEED * delta * airFactor
 		inputDir = -1.0
 	elif Input.is_action_pressed("right"):
 		velocity.x = SPEED * delta * airFactor
 		inputDir = 1.0
-	else:
-		if is_on_floor():
+		
+	else: # friction (instant)
+		if is_on_floor() and !wantsToPull and !wantsToPush:
 			velocity.x = 0
 	#jumpin
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_FORCE
+		
 	#gravity
-	if not is_on_floor():
+	if not is_on_floor() and !wantsToPull and !wantsToPush:
 		velocity.y += GRAVITY * delta
 
 		if is_on_floor(): FxManager.startLoopedFx('steps', footstepSound)
@@ -72,14 +76,11 @@ func groundedMovement(delta):
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_FORCE
 
-#func updateMagMovement():
-	#activeMagnetList = $magManager.selectables
-
 func magnetMovement(delta):
 	if flingMag and wantsToPull:
 		var vec_to_mag = flingMag.global_position - global_position
 		var dist = vec_to_mag.length()
-		
+		distanceScale = 1/dist
 		if dist < SNAP_DISTANCE: # snaps position to the magnet if you click and get real close
 			global_position = flingMag.global_position
 			velocity = Vector2.ZERO
@@ -87,8 +88,9 @@ func magnetMovement(delta):
 			
 		else: # 
 			var direction = vec_to_mag.normalized()
-			var magnetic_force = direction * MAGNET_FORCE
-			var acceleration = magnetic_force / MASS
+			var magForce = direction * MAGNET_FORCE * (log(1 + (distScaleConst / dist))/log(slopeScale))
+			print(magForce)
+			var acceleration = magForce / MASS
 			velocity += acceleration * delta
 
 
